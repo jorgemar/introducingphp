c8c15979a433065676fb118bbcfff4673d41d4de
jorgemar <jorge@jjmnetworks.com>
2013-11-17 23:27:49 -0800

    modified:   control_structures/switch.php

--- control_structures/switch.php
+++ control_structures/switch.php
@@ -14,5 +14,42 @@ switch ($name) {
 		break;
 }
 
+$a = 2;
+echo "<br>";
+switch ($a){
+    case 0:
+        echo "a equals 0<br>";
+        break;
+    case 1:
+        echo "a equals 1<br>";
+        break;
+     case 2:
+        echo "a equals 2<br>";
+        break;
+     case 3:
+        echo "a equals 3<br>";
+        break;
+    default:
+        echo "a is not equal to 1,2,3<br>";
+        break;
+}
 
+//chinese zodiac
+$year = 2013;
+
+switch (($year - 4) % 12){
+    case 0: $zodiac = "Rat"; break;
+    case 1: $zodiac = "Ox"; break;
+    case 2: $zodiac = "Tiger"; break;
+    case 3: $zodiac = "Rabbit"; break;
+    case 4: $zodiac = "Dragon"; break;
+    case 5: $zodiac = "Snake"; break;
+    case 6: $zodiac = "Horse"; break;
+    case 7: $zodiac = "Goat"; break;
+    case 8: $zodiac = "Monkey"; break;
+    case 9: $zodiac = "Rooster"; break;
+    case 10: $zodiac = "Dog"; break;
+    case 11: $zodiac = "Pig"; break;
+}
+echo "{$year} is the year of the {$zodiac}.";
 ?>
